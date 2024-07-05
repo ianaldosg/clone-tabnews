6b1036167b16a80f099a3167e37e83e27a4798a5
Ian Aldo Serwy Gonzales <106125819+ianaldosg@users.noreply.github.com>
2024-07-05 15:17:00 +0000

    adicionando scripts `lint:check´ e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto feito para aprendizado utilizando o curso do @filipedeschamps no site https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Eu te amo Staaar, LINDA MARAVILHOSA. To com sdd sua goxtosa</h1>
+  return <h1>Eu te amo Staaar, LINDA MARAVILHOSA. To com sdd sua goxtosa</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("teste2");
 }
 
 export default Home;
