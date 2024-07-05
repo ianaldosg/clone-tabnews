f8d52ed4fb9b82db3628fe27d7b70320a261d749
Ian Aldo Serwy Gonzales <106125819+ianaldosg@users.noreply.github.com>
2024-07-05 15:25:45 +0000

    adicionando `prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Eu te amo Staaar, LINDA MARAVILHOSA. To com sdd sua goxtosa</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("teste2");
-}
-
 export default Home;
