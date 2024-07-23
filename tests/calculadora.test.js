7ac5e12e03b81976c57fc1aee2aff68a2c6b4db9
Ian Aldo Serwy Gonzales <106125819+ianaldosg@users.noreply.github.com>
2024-07-23 18:46:00 +0000

    Exemplos de utilização de ´tests´ e ´models´

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -10,3 +10,17 @@ test("espero que 1 seja 1", () => {
   expect(1).toBe(1);
 });
 */
+const calculadora = require("../models/calculadora.js");
+
+test("somar 2 + 2 = 4", () => {
+  const resultado = calculadora.somar(2, 2);
+  expect(resultado).toBe(4);
+});
+test("somar 5 + 100 = 105", () => {
+  const resultado = calculadora.somar(5, 100);
+  expect(resultado).toBe(105);
+});
+test("somar 'banana' + 100 = 'Erro'", () => {
+  const resultado = calculadora.somar("banana", 100);
+  expect(resultado).toBe("Erro");
+});
