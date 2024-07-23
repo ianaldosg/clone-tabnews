fa2e00231f2caf48e81e8d0c46b39e39e0cf0215
Ian Aldo Serwy Gonzales <106125819+ianaldosg@users.noreply.github.com>
2024-07-23 18:08:26 +0000

    criação da pasta ´tests´

---
+++ tests/calculadora.test.js
@@ -0,0 +1,12 @@
+/*test("Tudo certo?", () => {
+  console.log("ainda to aqui!");
+});
+
+test("Mais uma vez tudo certo?", () => {
+  console.log("sim ainda aqui...");
+});
+
+test("espero que 1 seja 1", () => {
+  expect(1).toBe(1);
+});
+*/
